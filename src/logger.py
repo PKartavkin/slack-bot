7767994ff23affb847dcdfdb465983213ed37a00
Pavlo K. <pavel.kartavkin@ocelot.ai>
2025-12-06 11:12:39 +0400

    disabled mongo logs

--- src/logger.py
+++ src/logger.py
@@ -9,4 +9,11 @@ logging.basicConfig(
     handlers=[logging.StreamHandler(sys.stderr)]  # Changed to stderr for unbuffered output
 )
 
+# Suppress verbose pymongo DEBUG logs (topology, connection pool, etc.)
+# Set pymongo loggers to INFO level to reduce noise
+logging.getLogger("pymongo").setLevel(logging.INFO)
+logging.getLogger("pymongo.topology").setLevel(logging.INFO)
+logging.getLogger("pymongo.connection").setLevel(logging.INFO)
+logging.getLogger("pymongo.serverSelection").setLevel(logging.INFO)
+
 logger = logging.getLogger("slackbot")
