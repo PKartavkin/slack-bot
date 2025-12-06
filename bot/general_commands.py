4265f74914d465a21d0b7781bd6de1c8009661fc
Pavlo K. <pavel.kartavkin@ocelot.ai>
2025-12-06 23:01:22 +0400

    reformatted 'help'

--- bot/general_commands.py
+++ bot/general_commands.py
@@ -7,40 +7,40 @@ from bot.logger import logger
 def get_help() -> str:
     logger.debug("Help")
     return """
-    **Available commands:**
+    *Available commands:*
     
     *General:*
-    **help** - show this help message
-    **status** - show current channel status and project configuration
+    `help` - show this help message
+    `status` - show current channel status and project configuration
     
     *Project Management:*
-    **list projects** - list all available project configurations
-    **use project <name>** - bind channel to a project configuration
+    `list projects` - list all available project configurations
+    `use project <name>` - bind channel to a project configuration
     
     *Bug Reports:*
-    **create bug report** - format your message into a structured bug report
-    **show bug template** - show the current bug report template
-    **edit bug template** - edit the bug report template
+    `create bug report` - format your message into a structured bug report
+    `show bug template` - show the current bug report template
+    `edit bug template` - edit the bug report template
     
     *Documentation:*
-    **show project** - display project documentation/overview
-    **update docs** - update project documentation
-    **enable docs** - enable using project docs for bug reports
-    **disable docs** - disable using project docs for bug reports
+    `show project` - display project documentation/overview
+    `update docs` - update project documentation
+    `enable docs` - enable using project docs for bug reports
+    `disable docs` - disable using project docs for bug reports
     
     *Jira Configuration:*
-    **set jira token <token>** - set Jira API token
-    **set jira url <url>** - set Jira instance URL
-    **set jira email <email>** - set Jira email address
-    **set jira query <JQL>** - set JQL query for fetching bugs
-    **show jira query** - show current Jira JQL query
+    `set jira token <token>` - set Jira API token
+    `set jira url <url>` - set Jira instance URL
+    `set jira email <email>` - set Jira email address
+    `set jira query <JQL>` - set JQL query for fetching bugs
+    `show jira query` - show current Jira JQL query
     
     *Jira Default Fields:*
-    **set jira defaults field=value** - set Jira default field values (supports multiple: field1=value1 field2=value2)
-    **show jira defaults** - show all Jira default field values
-    **clear jira default <field>** - clear a Jira default field value
+    `set jira defaults field=value` - set Jira default field values (supports multiple: field1=value1 field2=value2)
+    `show jira defaults` - show all Jira default field values
+    `clear jira default <field>` - clear a Jira default field value
     
     *Jira Operations:*
-    **test jira** - test Jira connection for current project
-    **get bugs** - get list of Jira issues using the configured JQL query
+    `test jira` - test Jira connection for current project
+    `get bugs` - get list of Jira issues using the configured JQL query
     """
